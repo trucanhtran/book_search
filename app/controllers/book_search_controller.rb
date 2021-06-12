84d0a86753f0a65f83baad66751642d8a853901b
trucanh <anhtructran111@gmail.com>
2021-06-12 15:16:53 +0700

    add css jquery

--- app/controllers/book_search_controller.rb
+++ app/controllers/book_search_controller.rb
@@ -1,7 +1,10 @@
 class BookSearchController < ApplicationController
   def index
+    @categories = Category.all
   end
-  def search
 
+  def search
+    @products = Product.where("title LIKE ?", "%#{params[:keyword]}")
+    render json: @products
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,35 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("jquery");
+
+function appendText(data){
+  document.getElementById("id_products_list").innerHTML = "";
+  for (var i=0; i < data.length; i++){
+    var node = document.createElement("div");
+    var textNode = document.createTextNode(data[i].title);
+    node.appendChild(textNode);
+    document.getElementById("id_products_list").appendChild(node);
+  };
+};
+
+function displayProduct(data){
+  document.getElementById("id_content").innerHTML = ""
+  for (var i=0; i<data.length; i++){
+    var node = document.createElement("div");
+    var textNode = document.createTextNode(data[i].title);
+    node.appendChild(textNode);
+    document.getElementById("id_content").appendChild(node);
+  };
+};
+
+$(document).ready(function(){
+  $(document).on("keyup", "#id_search", function(event){
+    const keyword = event.target.value
+    $.post('/search', function(data){
+      appendText(data);
+      displayProduct(data);
+    });
+  });
+});
