baddba2be2b74af713d18bbea068dca149498181
trucanh <anhtructran111@gmail.com>
2021-06-27 13:33:40 +0700

    add ajax

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,9 @@ class ProductsController < ApplicationController
   def index
     @category = Category.find_by(id: params[:category_id])
     @products = @category.products.all
+    respond_to do |format|
+      format.js
+    end
   end
 
   def new
